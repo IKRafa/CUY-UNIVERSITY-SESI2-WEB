31dca0600562c75637b90d60fc7efb6f4ea41c5d
IKRafa <rafakadek14@gmail.com>
2024-09-15 22:04:54 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+const nama = "Rafa";
+let usia = 30;
+
+const biodata = document.getElementById('biodata');
+console.log(biodata);
+
+function generateBiodata() {
+let generasi;
+
+if(usia > 10 && usia < 18) {
+    generasi = "generasi remaja";
+}else if(usia >18 && usia < 30) { 
+    generasi = "generasi dewasa";
+}else if(usia >= 30){
+    generasi = "generasi lansia";
+}else if(usia < 10 && usia > 2){
+    generasi = "generasi anak-anak";
+}else {
+    generasi = "generasi bayi";
+    }
+
+return biodata.innerHTML = generasi;
+
+
+
+}
+
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
+
+
+
+
+
+
+
